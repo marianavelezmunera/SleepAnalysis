ff09e5685bed84b0730a8bb022e5698376d28fa8
Mariana <marianavelez.35@gmail.com>
2023-08-06 23:27:36 -0600

    commit

--- heatmap.R
+++ heatmap.R
@@ -20,4 +20,17 @@ ggplot(data=matbuena,aes(x=persona,y=sueno,fill=valor))+
   geom_tile(color = "white",
             lwd = 1.5,
             linetype = 1)+
-  coord_fixed()
+  coord_fixed()+
+  theme_pubclean()+
+  theme(legend.position = "right")+
+  xlab("Personal variables")+
+  ylab("Sleeping quality variables")+
+  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
+  theme(axis.text = element_text(family = "Open Sans"))+
+  scale_x_discrete(labels=c("Age","Job time","Job satisfaction","Self productivity"))+
+  scale_y_discrete(labels=c("Pittsburg index","Sleep hygiene","Insomnia index","Subth insomnia index"))+
+  theme(legend.title = element_text(family = "Open Sans",face="bold"))+
+  theme(legend.text = element_text(family = "Open Sans"))+
+  scale_fill_gradientn(name="Correlation",colors = met.brewer("Pissaro",6))
+  
+
