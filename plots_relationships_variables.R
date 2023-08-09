6e1b95d643870d1afdf1f4f8ded16d54047f8ef3
Mariana <marianavelez.35@gmail.com>
2023-08-08 21:14:44 -0600

    commit

--- plots_relationships_variables.R
+++ plots_relationships_variables.R
@@ -6,15 +6,16 @@ ggplot(data=DB,aes(x=job_satisfaction,y=self_productivity))+
   geom_point()+
   ylab("Self productivity")+xlab("Job satisfaction")+
   theme_pubclean()+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))+
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))+
   geom_smooth(method = "lm",fill=NA,aes(colour="A"))+
   scale_colour_manual(values=met.brewer("Pissaro",4)[3],label="rho = 0.36 ***")+
   theme(legend.position = c(0.8,0.5))+
   theme(legend.title = element_blank())+
   theme(legend.background = element_blank())+
   theme(legend.key = element_rect(fill = "transparent"))+
-  theme(legend.text = element_text(family = "Open Sans",size=10))
+  theme(legend.text = element_text(family = "Open Sans",size=24))
+ggsave("plot13.png")
 
 # tipo de trabajo vs satisfaccion
 
@@ -24,8 +25,9 @@ ggplot(data=DB,aes(x=job,y=job_satisfaction,fill=job))+
   theme_pubclean()+
   theme(legend.position = "none")+
   scale_fill_met_d("Pissaro")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))
+ggsave("plot14.png")
 
 # tipo de trabajo vs productividad
 
@@ -33,11 +35,11 @@ ggplot(data=DB,aes(x=job,y=self_productivity,fill=job))+
   geom_boxplot(color="black")+
   ylab("Self productivity")+xlab("Job")+
   theme_pubclean()+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))+
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))+
   theme(legend.position = "none")+
   scale_fill_met_d("Pissaro")
-  
+ggsave("plot15.png")  
 
 # shift vs productividad 
 
@@ -45,12 +47,12 @@ ggplot(data=DB,aes(x=shift,y=self_productivity,fill=shift))+
   geom_boxplot(color="black")+
   ylab("Self productivity")+xlab("Shift type")+
   theme_pubclean()+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))+
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))+
   theme(legend.position = "none")+
   scale_fill_met_d("Pissaro")+
   scale_x_discrete(labels=c("Night","Afternoon","Morning","Flexible"))
-  
+ggsave("plot16.png")  
 
 # shift vs satisfaction
 
@@ -58,11 +60,12 @@ ggplot(data=DB,aes(x=shift,y=job_satisfaction,fill=shift))+
   geom_boxplot(color="black")+
   ylab("Job satisfaction")+xlab("Shift type")+
   theme_pubclean()+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))+
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))+
   theme(legend.position = "none")+
   scale_fill_met_d("Pissaro")+
   scale_x_discrete(labels=c("Night","Afternoon","Morning","Flexible"))
+ggsave("plot17.png")
 
 # shift insomnio 
 
@@ -70,11 +73,12 @@ ggplot(data=DB,aes(x=shift,y=insomnio,fill=shift))+
   geom_boxplot(color="black")+
   ylab("Insomnio index")+xlab("Shift type")+
   theme_pubclean()+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))+
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))+
   theme(legend.position = "none")+
   scale_fill_met_d("Pissaro")+
   scale_x_discrete(labels=c("Night","Afternoon","Morning","Flexible"))
+ggsave("plot18.png")
 
 # Calidad del sueño vs productividad
 
@@ -82,11 +86,12 @@ ggplot(data=DB,aes(x=sleeping_quality,y=self_productivity,fill=sleeping_quality)
   geom_boxplot(color="black")+
   ylab("Self productivity")+xlab("Sleeping quality")+
   theme_pubclean()+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))+
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))+
   theme(legend.position = "none")+
   scale_fill_met_d("Pissaro")+
   scale_x_discrete(labels=c("Very bad","Bad","Good","Very good"))
+ggsave("plot19.png")
 
 # Calidad de sueño vs satisfaccion
 
@@ -94,28 +99,28 @@ ggplot(data=DB,aes(x=sleeping_quality,y=job_satisfaction,fill=sleeping_quality))
   geom_boxplot(color="black")+
   ylab("Job satisfaction")+xlab("Sleeping quality")+
   theme_pubclean()+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))+
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))+
   theme(legend.position = "none")+
   scale_fill_met_d("Pissaro")+
   scale_x_discrete(labels=c("Very bad","Bad","Good","Very good"))
-
+ggsave("plot20.png")
 # Insomnio vs satisfaccion
 
 ggplot(data=DB,aes(x=insomnio,y=job_satisfaction))+
   geom_point()+
   ylab("Insomnio index")+xlab("Job satisfaction")+
   theme_pubclean()+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))+
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))+
   geom_smooth(method = "lm",fill=NA,aes(colour="A"))+
   scale_colour_manual(values=met.brewer("Pissaro",4)[7],label="rho = 0.04")+
   theme(legend.position = c(0.8,0.65))+
   theme(legend.title = element_blank())+
   theme(legend.background = element_blank())+
   theme(legend.key = element_rect(fill = "transparent"))+
-  theme(legend.text = element_text(family = "Open Sans",size=10))
-
+  theme(legend.text = element_text(family = "Open Sans",size=24))
+ggsave("plot21.png")
 # Insomnio vs productividad
 
 ggplot(data=DB,aes(x=insomnio,y=self_productivity))+
--- plots_single_variable.R
+++ plots_single_variable.R
@@ -7,25 +7,28 @@ ggplot(data=DB,aes(x=age))+
   theme_pubclean()+
   ylab("Number of people")+
   xlab("Age (years)")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size=24))
+ggsave("plot1.png")
 
 ggplot(data = DB,aes(x=job))+
   geom_bar(width = 0.5,color="black",fill=met.brewer("Pissaro",10)[4])+
   theme_pubclean()+
   ylab("Number of people")+
   xlab("Job")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size=30))+
+  theme(axis.text = element_text(family = "Open Sans",size=24))
+ggsave("plot2.png")
 
 ggplot(data = DB,aes(x=sex))+
   geom_bar(width = 0.25,color="black",fill=met.brewer("Pissaro",10)[6])+
   theme_pubclean()+
   ylab("Number of people")+
   xlab("Sex")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))+
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size=30))+
+  theme(axis.text = element_text(family = "Open Sans",size=24))+
   scale_x_discrete(labels=c("Male","Female"))
+ggsave("plot3.png")
 
 # Sobre el trabajo
 
@@ -34,26 +37,30 @@ ggplot(data=DB,aes(x=time_job))+
   theme_pubclean()+
   ylab("Number of people")+
   xlab("Time job (years)")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size=30))+
+  theme(axis.text = element_text(family = "Open Sans",size=24))
+ggsave("plot4.png")
 
 ggplot(data = DB,aes(x=modalidad))+
   geom_bar(width = 0.25,color="black",fill=met.brewer("Pissaro",10)[9])+
   theme_pubclean()+
   ylab("Number of people")+
   xlab("Work type")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))+
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size=30))+
+  theme(axis.text = element_text(family = "Open Sans",size=24))+
   scale_x_discrete(labels=c("Remote","Hybrid","In-office"))
+ggsave("plot5.png")
+
 
 ggplot(data = DB, aes(x=shift))+
   geom_bar(width = 0.25,color="black",fill=met.brewer("Pissaro",10)[10])+
   theme_pubclean()+
   ylab("Number of people")+
   xlab("Shift type")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))+
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size=24))+
   scale_x_discrete(labels=c("Night","Afternoon","Morning","Flexible"))
+ggsave("plot6.png")
 
 ggplot(data=DB,aes(x=jobdays_perweek))+
   geom_histogram(bins =7,color="black",fill=met.brewer("Pissaro",10)[8])+
@@ -61,8 +68,9 @@ ggplot(data=DB,aes(x=jobdays_perweek))+
   theme_pubclean()+
   ylab("Number of people")+
   xlab("Job days per week")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size=24))
+ggsave("plot7.png")
 
 # Sobre el sueño
 
@@ -71,38 +79,43 @@ ggplot(data=DB,aes(x=sleeping_quality))+
   theme_pubclean()+
   ylab("Number of people")+
   xlab("Sleeping quality")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))+
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))+
   scale_x_discrete(labels=c("Very bad","Bad","Good","Very good"))
+ggsave("plot8.png")
 
 ggplot(data=DB,aes(x=job_satisfaction))+
   geom_histogram(color="black",fill=met.brewer("Pissaro",10)[1])+
   theme_pubclean()+
   ylab("Number of people")+
   xlab("Job satisfaction")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))
+ggsave("plot9.png")
 
 ggplot(data=DB,aes(x=self_productivity))+
   geom_histogram(color="black",fill=met.brewer("Pissaro",10)[3])+
   theme_pubclean()+
   ylab("Number of people")+
   xlab("Self productivity")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))
+ggsave("plot10.png")
 
 ggplot(data=DB,aes(x=hygine))+
   geom_histogram(color="black",fill=met.brewer("Pissaro",10)[2])+
   theme_pubclean()+
   ylab("Number of people")+
   xlab("Sleep hygiene")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))
+ggsave("plot11.png")
 
 ggplot(data=DB,aes(x=insomnio))+
   geom_histogram(color="black",fill=met.brewer("Pissaro",10)[5])+
   theme_pubclean()+
   ylab("Number of people")+
   xlab("Insomnia index")+
-  theme(axis.title = element_text(face="bold",family = "Open Sans"))+
-  theme(axis.text = element_text(family = "Open Sans"))
+  theme(axis.title = element_text(face="bold",family = "Open Sans",size = 30))+
+  theme(axis.text = element_text(family = "Open Sans",size = 24))
+ggsave("plot12.png")
